301c60632766ef61d10baaad492a0c8473845895
PNguyen <ngocphunguyenabc@gmail.com>
2024-10-09 17:02:38 +0700

    refactor: :recycle: fix margin of editable text area in NoteEditor
    Author: PNguyen
    Signed-off-by: PNguyen <ngocphunguyenabc@gmail.com>

--- src/main/java/com/note_awesome/views/core_editors/NoteEditorFxController.java
+++ src/main/java/com/note_awesome/views/core_editors/NoteEditorFxController.java
@@ -3,6 +3,7 @@ package com.note_awesome.views.core_editors;
 import com.note_awesome.NoteAwesomeFX;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.IndexRange;
 import javafx.scene.layout.VBox;
@@ -17,22 +18,23 @@ import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
-public class NoteEditorFxController extends VBox{
-    
+public class NoteEditorFxController extends VBox {
+
     private final FoldableStyleArea area = new FoldableStyleArea();
-    
-    
+
+
     @FXML
     private Button boldBtn;
-    
+
     @FXML
     private Button italicBtn;
-    
+
     @FXML
     private Button underlineBtn;
-    
+
     public NoteEditorFxController() {
-        try{
+        super();
+        try {
             FXMLLoader loader = new FXMLLoader(NoteAwesomeFX.class.getResource("fxml/note_views/NoteEditor.fxml"));
             loader.setRoot(this);
             loader.setController(this);
@@ -41,80 +43,80 @@ public class NoteEditorFxController extends VBox{
             throw new RuntimeException(e);
         }
     }
-    
+
     @FXML
     private void initialize() {
         // Add the area to the root VBox
         VirtualizedScrollPane<GenericStyledArea<ParStyle, Either<String, LinkedImage>, TextStyle>> vsPane = new VirtualizedScrollPane<>(area);
         VBox.setVgrow(vsPane, javafx.scene.layout.Priority.ALWAYS);
+        VBox.setMargin(vsPane, new Insets(0, 10, 0, 10));
         getChildren().add(1, vsPane);
         // assign event handlers
         boldBtn.setOnAction(e -> {
             toggleBold();
             area.requestFocus();
         });
-        
+
         italicBtn.setOnAction(e -> {
             toggleItalic();
             area.requestFocus();
         });
-        
+
         underlineBtn.setOnAction(e -> {
             toggleUnderline();
             area.requestFocus();
         });
-        
-        
+
+
         // Add listeners to area
         area.beingUpdatedProperty().addListener((obs, old, beingUpdated) -> {
-            if(!beingUpdated) {
+            if (!beingUpdated) {
                 boolean bold, italic, underline;
                 
-                
                 IndexRange selection = area.getSelection();
-                
-                if(selection.getLength() != 0) {
+
+                if (selection.getLength() != 0) {
                     StyleSpans<TextStyle> styles = area.getStyleSpans(selection);
-                    bold = styles.styleStream().allMatch(style -> style.bold.orElse(false));
-                    italic = styles.styleStream().allMatch(style -> style.italic.orElse(false));
-                    underline = styles.styleStream().allMatch(style -> style.underline.orElse(false));
+                    bold = styles.styleStream().allMatch(style -> style.bold.orElse(Boolean.valueOf(false)));
+                    italic = styles.styleStream().allMatch(style -> style.italic.orElse(Boolean.valueOf(false)));
+                    underline = styles.styleStream().allMatch(style -> style.underline.orElse(Boolean.valueOf(false)));
                 } else {
                     int paragraph = area.getCurrentParagraph();
                     int column = area.getCaretColumn();
                     TextStyle style = area.getStyleAtPosition(paragraph, column);
-                    
-                    bold = style.bold.orElse(false);
-                    italic = style.italic.orElse(false);
-                    underline = style.underline.orElse(false);
+
+                    bold = style.bold.orElse(Boolean.valueOf(false));
+                    italic = style.italic.orElse(Boolean.valueOf(false));
+                    underline = style.underline.orElse(Boolean.valueOf(false));
                 }
-                
+
                 int startPar = area.offsetToPosition(selection.getStart(), TwoDimensional.Bias.Forward).getMajor();
                 int endPar = area.offsetToPosition(selection.getEnd(), TwoDimensional.Bias.Backward).getMajor();
-                List< Paragraph<ParStyle, Either<String, LinkedImage>, TextStyle>> pars = area.getParagraphs().subList(startPar, endPar);
+                List<Paragraph<ParStyle, Either<String, LinkedImage>, TextStyle>> pars = area.getParagraphs().subList(startPar, endPar);
             }
         });
-        
+
     }
 
     private void updateStyleInSelection(Function<StyleSpans<TextStyle>, TextStyle> mixinGetter) {
         IndexRange selection = area.getSelection();
-        if(selection.getLength() != 0) {
+        if (selection.getLength() != 0) {
             StyleSpans<TextStyle> styles = area.getStyleSpans(selection);
             TextStyle mixin = mixinGetter.apply(styles);
             StyleSpans<TextStyle> newStyles = styles.mapStyles(style -> style.updateWith(mixin));
             area.setStyleSpans(selection.getStart(), newStyles);
         }
     }
-    
+
     private void toggleBold() {
-        updateStyleInSelection(spans -> TextStyle.bold(!spans.styleStream().allMatch(style -> style.bold.orElse(false))));
+        updateStyleInSelection(spans -> TextStyle.bold(!spans.styleStream().allMatch(style -> style.bold.orElse(Boolean.valueOf(false)))));
     }
-    
+
     private void toggleItalic() {
-        updateStyleInSelection(spans -> TextStyle.italic(!spans.styleStream().allMatch(style -> style.italic.orElse(false))));
+        updateStyleInSelection(spans -> TextStyle.italic(!spans.styleStream().allMatch(style -> style.italic.orElse(Boolean.valueOf(false)))));
     }
-    
+
     private void toggleUnderline() {
-        updateStyleInSelection(spans -> TextStyle.underline(!spans.styleStream().allMatch(style -> style.underline.orElse(false))));
+        updateStyleInSelection(spans -> TextStyle.underline(!spans.styleStream().allMatch(style -> style.underline.orElse(Boolean.valueOf(false)))));
     }
 }
